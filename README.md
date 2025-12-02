d9b9b6b5fa412639593437f62b8206fb9d22aff1
CristoJV <44086220+CristoJV@users.noreply.github.com>
2025-12-02 21:15:38 +0100

    feat(cam): Plot retrieved signal from N consecutive frames

--- README.md
+++ README.md
@@ -8,3 +8,13 @@ Using uv and system-packages
 curl -LsSf https://astral.sh/uv/install.sh | sh
 uv venv --system-site-packages --python /usr/bin/python3
 ```
+# Hardware
+Using Raspberry Pi Camera Module v3
+| Param | Value |
+| ----  | ----  |
+| Still resolution | 4608x2592 |
+| Sensor size  / diagonal | 7.4mm (1/2.43") |
+| Pixel size | 1.4um x 1.4 um |
+
+## Evaluation
+Min exposure time: 36us @ 1280x720 - Filtered BW 27.777 kHz - 13.9kHz
--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import time
+from typing import Any, Dict
 
 import cv2
+import matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
 from libcamera import controls
-from picamera2 import Picamera2, Preview
+from picamera2 import Picamera2
+
+matplotlib.use("TkAgg")
 
 # Initialize Camera
 cam = Picamera2()
-
+print(cam.sensor_modes)
 # Configuration
 config = cam.create_preview_configuration(
     main={
-        "size": (1280, 720),
+        "size": (1536, 864),
         "format": "RGB888",
     },
     lores={"size": (320, 240), "format": "YUV420"},
@@ -26,9 +31,9 @@ print(props)
 
 controls_dict = {
     "AeEnable": False,
-    "AnalogueGain": 2.0,
-    "ExposureTime": 100,
-    "FrameDurationLimits": (16666, 33333),
+    "AnalogueGain": 1.0,
+    "ExposureTime": 8,
+    "FrameDurationLimits": (8333, 8333),
     "AwbEnable": False,
     "AwbMode": controls.AwbModeEnum.Auto,
     "AfMode": controls.AfModeEnum.Auto,
@@ -49,31 +54,42 @@ cam_controls = cam.camera_controls
 for name, (minv, maxv, current) in cam_controls.items():
     print(f"{name:25s}  min={minv}  max={maxv}  default={current}")
 
-while True:
+
+# Take N consecutive frames
+MAX_FRAMES = 10
+
+frame = cam.capture_array("main")
+height, width = frame.shape[:2]
+frames = np.zeros((MAX_FRAMES, height, width))
+for i in range(MAX_FRAMES):
     frame = cam.capture_array("main")
-    meta = cam.capture_metadata()
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    frames[i, :, :] = frame
 
-    fps = 1e6 / meta.get("FrameDuration", 33333)
-    exp = meta.get("ExposureTime", 0) / 1000.0
-    ag = meta.get("AnalogueGain", -1)
+stacked_frames = frames.reshape(height * MAX_FRAMES, width)
 
-    text = f"FPS: {fps:.1f} Exp:{exp:.2f}ms AG:{ag:.2f}"
-    cv2.putText(
-        frame,
-        text,
-        (10, 30),
-        cv2.FONT_HERSHEY_SIMPLEX,
-        0.7,
-        (0, 255, 0),
-        2,
-        cv2.LINE_AA,
-    )
-    cv2.imshow("Camera Preview", frame)
+averaged_frames = np.mean(stacked_frames, axis=1)
 
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
+fig, ax = plt.subplots(1, 2, figsize=(16, 8), constrained_layout=True)
+ax[0].plot(averaged_frames)
+ax[0].set_title(f"Column averaged signal of {MAX_FRAMES} consecutive frames")
+ax[0].set_box_aspect(height / width)
 
-cam.stop()
-cv2.destroyAllWindows()
+meta: Dict[str, Any] = cam.capture_metadata()
 
-print("End capture.")
+fps = 1e6 / meta.get("FrameDuration", 33333)
+exp = meta.get("ExposureTime", 0) / 1000.0
+ag = meta.get("AnalogueGain", -1)
+
+ax[1].imshow(frame)
+ax[1].set_title(f"FPS: {fps:.1f} Exp:{exp:.4f}ms AG:{ag:.2f}")
+
+
+def on_key(event):
+    print(f"Pressed: {event.key}")
+    if event.key == "q":
+        plt.close(fig)
+
+
+fig.canvas.mpl_connect("key_press_event", on_key)
+plt.show()
